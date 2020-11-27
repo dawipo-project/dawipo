cd3a9bd0ff4d6e64e85f135e0a72b2dd520b4efd
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-11-27 14:11:18 -0500

    Fixed typos and indicators

--- contacts/models.py
+++ contacts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=40)
-	email = models.EmailField()
+	email = models.EmailField(unique=True)
 	phone_number = models.CharField(max_length=20)
 	organization = models.CharField(max_length=40)
 	position = models.CharField(max_length=40)

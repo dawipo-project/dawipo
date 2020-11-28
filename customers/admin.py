e6268c17c97f403a8301af8d5af404d21d43da4f
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-11-27 21:52:34 -0500

    Updated customers admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from.models import Customer
+from.models import Customer, CustomerContact
 
 # Register your models here.
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
 	list_display = ['name', 'city', 'first_name', 'last_name', 'email']
 	list_filter = ['name', 'city']
+
+@admin.register(CustomerContact)
+class CustContactAdmin(admin.ModelAdmin):
+	list_display = ['contact', 'company']
+	list_filter = ['company']

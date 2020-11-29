49caab9d52e0f2a7469c97ccf8a313cb1902c30c
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-11-29 12:26:11 -0500

    Added Export to CSV function

--- catalog/models.py
+++ catalog/models.py
@@ -28,13 +28,14 @@ class Product(models.Model):
 	category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, db_index=True)
 	slug = models.SlugField(max_length=200, db_index=True)
-	sku = models.CharField(max_length=20, db_index=True, blank=True)
+	sku = models.CharField(max_length=20, db_index=True, blank=True, null=True)
 	image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
 	description = models.TextField(blank=True)
-	color = models.CharField(max_length=15, blank=True)
-	measures = models.TextField(blank=True)
-	retail_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-	whole_sale_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+	observations = models.TextField(blank=True)
+	color = models.CharField(max_length=15, blank=True) # Observaciones
+	measures = models.TextField(blank=True) # Observaciones
+	retail_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True) # Separador de miles
+	whole_sale_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True) # Separador de miles
 	available = models.BooleanField(default=True)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
@@ -48,6 +49,9 @@ class Product(models.Model):
 
 	def save(self):
 		self.slug = slugify(self.name)
+		super(Product, self).save(commit=False)
+		if self.sku:
+			self.sku = str(self.category.id) + str(self.id)
 		super(Product, self).save()
 
 	def __str__(self):

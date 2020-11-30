c3e97756da661b9fd23a4d1fbfd47bff4b49d844
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-11-30 17:36:43 -0500

    Removed account prefix from urls

--- catalog/views.py
+++ catalog/views.py
@@ -95,7 +95,6 @@ def create_product(request):
 		'categories': categories, 'customers': customers})
 
 class ProductList(ListView):
-	paginate_by = 10
 	model = Product
 	context_object_name = 'products'
 	template_name = 'catalog/product/product_list.html'

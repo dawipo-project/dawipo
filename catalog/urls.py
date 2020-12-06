6f630c26048efd42aacb197dd1f263206f4439f6
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-06 05:23:54 -0500

    Added Excep export view to Catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
+	path('catalog/export_csv/', views.export_csv, name='export_all_csv'),
 	path('catalog/list/', views.ProductList.as_view(), name='product_list_table'),
 	# path('catalog/add/', views.ProductCreate.as_view(), name='add_product'),
 	path('catalog/add/', views.create_product, name='add_product'),
@@ -13,5 +14,4 @@ urlpatterns = [
 	path('catalog/', views.product_list, name='product_list'),
 	path('catalog/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 	path('catalog/<int:id>/<slug:slug>/edit/', views.edit, name='product_edit'),
-	path('catalog/export_csv/', views.export_csv, name='export_all_csv'),
 ]

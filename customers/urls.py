a9fe3f557ff256f3da80ee8905a3da2ed41844fb
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-05 19:49:00 -0500

    Added customer detail view

--- customers/urls.py
+++ customers/urls.py
@@ -6,6 +6,6 @@ app_name = 'customers'
 urlpatterns = [
 	path('create/', views.CustomerRegistrationView.as_view(), name='customer_registration'),
 	path('list/', views.CustomerList.as_view(), name='customer_list'),
-	path('<int:customer_id>/', views.CustomerDetail.as_view(), name='cust_detail'),
+	path('<int:pk>/', views.CustomerDetail.as_view(), name='cust_detail'),
 	path('import/', views.import_csv, name='import_csv'),
 ]

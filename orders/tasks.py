09ff2cb99c4a2e6f41a62b036e9ecc39a652840f
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-06 08:01:48 -0500

    nth attempt to fix emails

--- orders/tasks.py
+++ orders/tasks.py
@@ -7,7 +7,7 @@ def order_created(order_id, email):
 	order = Order.objects.get(id=order_id)
 	subject = f'Order No. {order.id}'
 	message = f'Dear {order.customer.first_name} {order.customer.last_name},\n\nA pre-order has been placed in our system for {order.customer.name}, which you requested.\nYour order id is {order.id}'
-	mail_sent = send_mail(subject, message, 'dawipoproject@gmail.com', [order.customer.email, email])
+	mail_sent = send_mail(subject, message, 'no_reply@dawipo.co', [order.customer.email, email])
 	return mail_sent
 
 @task
@@ -15,5 +15,5 @@ def order_edited(order_id, new_status):
 	order = Order.objects.get(id=order_id)
 	subject = f'Changes on Order No. {order.id}'
 	message = f'Dear {order.customer.first_name} {order.customer.last_name},\n\nYour order No. {order.id} has been updated in our system, and its new status is {new_status}.'
-	mail_sent = send_mail(subject, message, 'dawipoproject@gmail.com', [order.customer.email])
+	mail_sent = send_mail(subject, message, 'no_reply@dawipo.co', [order.customer.email])
 	return mail_sent

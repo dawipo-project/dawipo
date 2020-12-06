3457793c869c1aea2fd9b88c2d127c074c123c41
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-06 07:44:12 -0500

    Fixed some url bullshit

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
 	# path('catalog/add/', views.ProductCreate.as_view(), name='add_product'),
 	path('catalog/add/', views.create_product, name='add_product'),
 	path('catalog/category/add/', views.CategoryCreate.as_view(), name='add_category'),
+	path('catalog/', views.product_list, name='product_list'),
 	path('catalog/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
 	path('catalog/<slug>/list/', views.CategoryProductList.as_view(), name='category_product_list_table'),
-	path('catalog/', views.product_list, name='product_list'),
 	path('catalog/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 	path('catalog/<int:id>/<slug:slug>/edit/', views.edit, name='product_edit'),
 ]

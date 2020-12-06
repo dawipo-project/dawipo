c8ffbb61f165f4ff181e50304d78a9d3912ebfe4
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-05 21:39:33 -0500

    Added CSV Export view to catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
 	path('catalog/', views.product_list, name='product_list'),
 	path('catalog/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 	path('catalog/<int:id>/<slug:slug>/edit/', views.edit, name='product_edit'),
-	path('catalog/export_csv/', views.export_csv, name='export_csv'),
+	path('catalog/export_csv/', views.export_csv, name='export_all_csv'),
+	path('catalog/<slug:slug>/export_csv/', views.export_csv, name='export_csv'),
 ]

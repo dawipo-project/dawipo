2bb58fae7ee04de829cb147a0db8248da88521e5
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-07 18:00:28 -0500

    Fixed order pdf

--- orders/models.py
+++ orders/models.py
@@ -84,7 +84,7 @@ class Order(models.Model):
 			return 0
 
 	def get_total_cost(self):
-		return round((self.get_cost() + self.get_total_tax(), 2) + self.shipping)
+		return round((self.get_cost() + self.get_total_tax() + self.shipping),2)
 
 class OrderChange(models.Model):
 	order = models.ForeignKey(Order, related_name='order_order_changes', on_delete=models.CASCADE)

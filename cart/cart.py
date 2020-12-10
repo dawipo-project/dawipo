299511e61b666dacba151a36428d6bf19da9a5b1
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-10 15:31:16 -0500

    Added select field for price in order product list

--- cart/cart.py
+++ cart/cart.py
@@ -10,10 +10,10 @@ class Cart(object):
 			cart = self.session[settings.CART_SESSION_ID] = {}
 		self.cart = cart
 
-	def add(self, product, quantity=1, override_quantity=False):
+	def add(self, product, price, quantity=1, override_quantity=False):
 		product_id = str(product.id)
 		if product_id not in self.cart:
-			self.cart[product_id] = {'quantity': 0, 'price': str(product.price_1), 'tax': str(product.tax)}
+			self.cart[product_id] = {'quantity': 0, 'price': str(price), 'tax': str(product.tax)}
 		if override_quantity:
 			self.cart[product_id]['quantity'] = quantity
 		else:

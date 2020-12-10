a42ea9344331676d7aaf7d22d097fec9651fdb77
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-10 15:42:10 -0500

    Added select field for price in order product list

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,6 +10,11 @@ class ProductEditForm(forms.ModelForm):
         	'observations', 'brand', 'color', 'provider', 
             'price_1', 'price_2', 'price_3', 'barcode', 'measures',
             'fabrication_cost', 'tax', 'available')
+        price_1 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+        price_2 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+        price_3 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+        fabrication_cost = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+        tax = forms.DecimalField(decimal_places=2, localize=True, blank=True)
 
 class ProductCreateForm(forms.ModelForm):
 	class Meta:
@@ -18,6 +23,11 @@ class ProductCreateForm(forms.ModelForm):
             'observations', 'brand', 'color', 'provider', 
             'price_1', 'price_2', 'price_3', 'barcode', 'measures',
             'fabrication_cost', 'tax', 'available')
+        price_1 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+        price_2 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+        price_3 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+        fabrication_cost = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+        tax = forms.DecimalField(decimal_places=2, localize=True, blank=True)
 
 class CategoryForm(forms.ModelForm):
 	class Meta:

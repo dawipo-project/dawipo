cbb2367e44e7356c8d7e32ef94ad7b7f508d1f40
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-10 15:44:35 -0500

    Added select field for price in order product list

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,30 +4,32 @@ from .models import Category, Product
 
 # Formulario de autenticación de usuarios
 class ProductEditForm(forms.ModelForm):
+    price_1 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+    price_2 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+    price_3 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+    fabrication_cost = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+    tax = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+
     class Meta:
         model = Product
         fields = ('category', 'name', 'sku', 'image', 'description', 
         	'observations', 'brand', 'color', 'provider', 
             'price_1', 'price_2', 'price_3', 'barcode', 'measures',
             'fabrication_cost', 'tax', 'available')
-        price_1 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
-        price_2 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
-        price_3 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
-        fabrication_cost = forms.DecimalField(decimal_places=2, localize=True, blank=True)
-        tax = forms.DecimalField(decimal_places=2, localize=True, blank=True)
 
 class ProductCreateForm(forms.ModelForm):
-	class Meta:
+    price_1 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+    price_2 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+    price_3 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+    fabrication_cost = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+    tax = forms.DecimalField(decimal_places=2, localize=True, blank=True)
+
+    class Meta:
 		model = Product
 		fields = ('category', 'name', 'sku', 'image', 'description', 
             'observations', 'brand', 'color', 'provider', 
             'price_1', 'price_2', 'price_3', 'barcode', 'measures',
             'fabrication_cost', 'tax', 'available')
-        price_1 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
-        price_2 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
-        price_3 = forms.DecimalField(decimal_places=2, localize=True, blank=True)
-        fabrication_cost = forms.DecimalField(decimal_places=2, localize=True, blank=True)
-        tax = forms.DecimalField(decimal_places=2, localize=True, blank=True)
 
 class CategoryForm(forms.ModelForm):
 	class Meta:

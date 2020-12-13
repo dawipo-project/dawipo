fbe84765b3fd3ef8886ee6214fd9775357a6e75f
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-13 15:39:15 -0500

    Adding to cart does not redirect anymore

--- cart/views.py
+++ cart/views.py
@@ -11,7 +11,6 @@ def cart_add(request, product_id):
 	cart = Cart(request)
 	product = get_object_or_404(Product, id=product_id)
 	form = CartAddProductForm(request.POST)
-	import pdb; pdb.set_trace()
 	if form.is_valid():
 		cd = form.cleaned_data
 		cart.add(product=product, quantity=cd['quantity'], price=cd['price'], override_quantity=cd['override'])

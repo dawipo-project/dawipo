59d25bf4d7bd989fb9e49a988040644df6b1ea22
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-13 20:19:53 -0500

    Fixed order cost calculation

--- orders/models.py
+++ orders/models.py
@@ -101,10 +101,10 @@ class Order(models.Model):
 			return 0
 
 	def get_total_cost(self):
-		if self.discount:
-			return round(((self.get_cost() + self.get_total_tax()) * (self.discount / 100) + self.shipping), 2)
+		if self.discount > 0:
+			return round((((self.get_cost() + self.get_total_tax()) * (self.discount / 100)) + self.shipping), 2)
 		else:
-			return round((self.get_cost() + self.get_total_tax()), 2)
+			return round(((self.get_cost() + self.get_total_tax()) + self.shipping), 2)
 
 class OrderChange(models.Model):
 	order = models.ForeignKey(Order, related_name='order_order_changes', on_delete=models.CASCADE)

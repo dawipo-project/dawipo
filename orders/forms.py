85d66485a722672561613f3a73d112b171665c0d
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-13 20:12:25 -0500

    Added discount field to Order model

--- orders/forms.py
+++ orders/forms.py
@@ -5,12 +5,13 @@ from company.models import Company
 class OrderCreateForm(forms.ModelForm):
 	due_date = forms.DateField(localize=True)
 	shipping = forms.DecimalField(decimal_places=2, localize=True)
+	discount = forms.DecimalField(decimal_places=2, required=False, localize=True)
 
 	class Meta:
 		model = Order
 		fields = ['payment_method', 'customer', 'due_date', 'incoterm', 
 		'delivery_site', 'transport', 'country_of_origin', 
-		'tax', 'shipping', 'observations']
+		'tax', 'shipping', 'observations', 'discount']
 		exclude = ('company',)
 
 class OrderEditForm(forms.ModelForm):
--- orders/models.py
+++ orders/models.py
@@ -101,7 +101,7 @@ class Order(models.Model):
 			return 0
 
 	def get_total_cost(self):
-		return round(((self.get_cost() + self.get_total_tax() + self.shipping) * (self.discount / 100)), 2)
+		return round(((self.get_cost() + self.get_total_tax()) * (self.discount / 100) + self.shipping), 2)
 
 class OrderChange(models.Model):
 	order = models.ForeignKey(Order, related_name='order_order_changes', on_delete=models.CASCADE)

22ffbf164109eec32097de2d02d0b40b90a20576
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-16 15:51:11 -0500

    Fixed unit prices in order pdf

--- orders/models.py
+++ orders/models.py
@@ -102,7 +102,7 @@ class Order(models.Model):
 
 	def get_total_discount(self):
 		if self.discount > 0:
-			return round((self.get_cost() + self.get_total_tax()) * (self.discount / 100), 2)
+			return round(self.get_cost() * (self.discount / 100), 2)
 		else:
 			return 0
 

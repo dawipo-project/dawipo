2ab91dcb8c7e6c14eaa106a67c7a10b2636354f2
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-17 20:08:26 -0500

    fixed shipping field

--- orders/views.py
+++ orders/views.py
@@ -59,16 +59,14 @@ def order_create(request):
 			order = form.save(commit=False)
 			order.user = request.user
 			order.company = request.user.profile.company
-			import pdb; pdb.set_trace()
 			if order.discount is None:
-				order.discount = Decimal(0)
+				order.discount = 0
 			if order.shipping is None:
-				order.shipping = Decimal(0)
-			pdb.set_trace()
+				order.shipping = 0
 			order.save()
 			for item in cart:
 				if item['tax'] is None:
-					tax = Decimal(0)
+					tax = 0
 				else:
 					tax = item['tax']
 				OrderItem.objects.create(order=order,

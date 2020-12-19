8df2bc95c9c48383fa9fb18c6eaccfe3fdd0fe6c
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-19 13:04:44 -0500

    Attempt to fix orders

--- cart/cart.py
+++ cart/cart.py
@@ -36,6 +36,7 @@ class Cart(object):
 		for product in products:
 			cart[str(product.id)]['product'] = product
 		for item in cart.values():
+			item['tax'] = float(item['tax'])
 			import pdb; pdb.set_trace()
 			item['price'] = float(item['price'])
 			item['total_tax'] = item['tax'] * item['quantity']

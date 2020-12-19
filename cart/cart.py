3c1e5b9b0b08073b8435bd9ac97767e1d4c11177
Felipe Esteban Hernandez <felipehb@MacBook-Pro-de-Felipe.local>
2020-12-19 12:35:09 -0500

    Attempt to fix orders

--- cart/cart.py
+++ cart/cart.py
@@ -45,10 +45,10 @@ class Cart(object):
 		return sum(item['quantity'] for item in self.cart.values())
 
 	def get_total_price(self):
-		return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+		return sum(item['price'] * item['quantity'] for item in self.cart.values())
 
 	def get_total_tax(self):
-		return sum(Decimal(item['tax']) * item['quantity'] for item in self.cart.values())
+		return sum(item['tax'] * item['quantity'] for item in self.cart.values())
 
 	def clear(self):
 		del self.session[settings.CART_SESSION_ID]
